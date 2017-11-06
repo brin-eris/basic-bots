6935d11fd7a846372d614915748b934e93baf611
t riffix rex <brian.eugene.pollard@gmail.com>
2017-11-06 04:12:55 -0500

    start somewhere

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, './index.html');
+
+// define routes and socket
+const server = express();
+server.get('/', function(req, res) { res.sendFile(INDEX); });
+server.use('/', express.static(path.join(__dirname, '.')));
+let requestHandler = server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const io = socketIO(requestHandler);
---
+++ src/client/clientMain.js
@@ -0,0 +1,34 @@
+
+// module aliases
+const    Engine = require('matter-js').Engine;
+const    Render = require('matter-js').Render;
+const    World = require('matter-js').World;
+const    Bodies = require('matter-js').Bodies;
+
+document.addEventListener('DOMContentLoaded', function(e) {
+
+
+  // create an engine
+  var engine = Engine.create();
+
+  // create a renderer
+  var render = Render.create({
+      element: document.body,
+      engine: engine
+  });
+
+  // create two boxes and a ground
+  var boxA = Bodies.rectangle(400, 200, 80, 80);
+  var boxB = Bodies.rectangle(450, 50, 80, 80);
+  var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+  // add all of the bodies to the world
+  World.add(engine.world, [boxA, boxB, ground]);
+
+  // run the engine
+  Engine.run(engine);
+
+  // run the renderer
+  Render.run(render);
+
+});
